57ca3be33091c8e258189eef922ca6180ef7f09b
kavvata <gabriel.kawata@gmail.com>
2026-01-28 15:15:06 -0300

    added openvpn to networkmanager in think

--- hosts/think/configuration.nix
+++ hosts/think/configuration.nix
@@ -258,7 +258,10 @@ in {
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      plugins = with pkgs; [ networkmanager-openvpn ];
+    };
 
   };
 
