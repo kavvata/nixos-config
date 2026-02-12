0241f891213b1c515420f65db7cdb51b9b59afee
kavvata <gabriel.kawata@gmail.com>
2026-02-12 08:10:18 -0300

    created common services module

---
+++ hosts/common/packages/services.nix
@@ -0,0 +1,32 @@
+{ ... }:
+{
+  niriServices = {
+    gvfs.enable = true;
+    udisks2.enable = true;
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    xserver.enable = false;
+
+    flatpak.enable = true;
+    tlp.enable = true;
+
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+
+    };
+  };
+}
