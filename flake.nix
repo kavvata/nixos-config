26d7c1b1ebd305688b152cfa4a190b29993d8fd2
kavvata <gabriel.kawata@gmail.com>
2026-02-19 12:45:27 -0300

    formatting

--- flake.nix
+++ flake.nix
@@ -1,30 +1,40 @@
 {
   description = "Nixos config flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-
-      bookling = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ({ _, ... }: {
-            nixpkgs.overlays = [ (import ./overlays/helium.nix) ];
-          })
-	./hosts/bookling/configuration.nix 
-	];
-      };
-      think = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          ({ _, ... }: {
-            nixpkgs.overlays = [ (import ./overlays/helium.nix) ];
-          })
-          ./hosts/think/configuration.nix
-        ];
-      };
-
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations = {
+
+        bookling = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            (
+              { _, ... }:
+              {
+                nixpkgs.overlays = [ (import ./overlays/helium.nix) ];
+              }
+            )
+            ./hosts/bookling/configuration.nix
+          ];
+        };
+        think = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            (
+              { _, ... }:
+              {
+                nixpkgs.overlays = [ (import ./overlays/helium.nix) ];
+              }
+            )
+            ./hosts/think/configuration.nix
+          ];
+        };
+
+      };
+    };
 }
