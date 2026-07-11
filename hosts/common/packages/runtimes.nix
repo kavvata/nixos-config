a4f77bded98339889ec710a7879614ab07405cc7
kavvata <gabriel.kawata@gmail.com>
2026-07-11 09:44:55 -0300

    adds comanteries to runtimes

--- hosts/common/packages/runtimes.nix
+++ hosts/common/packages/runtimes.nix
@@ -5,12 +5,15 @@ pkgs: with pkgs; [
   go
   pipenv
   nodejs_24
+  # NOTE: for vterm, but i'm not using vterm anymore for emacs
   cmake
   libtool
+  # NOTE: for lsp and formatting in emacs
   nixfmt
   nil
   ty
   ruff
   shellcheck
+  # NOTE: for graph rendering in org-roam
   graphviz
 ]
